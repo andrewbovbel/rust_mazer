5aa3276559d19abbcb5fa738391ec2db0fa1c815
Andrew Bovbel <andrew@bovbel.com>
2024-07-13 15:02:34 -0400

    set up bare bones

---
+++ target/debug/deps/rust_mazer-3adde574d2ea8e65.d
@@ -0,0 +1,5 @@
+/Users/andrewbovbel/Developer/rust_mazer/target/debug/deps/rust_mazer-3adde574d2ea8e65: src/main.rs
+
+/Users/andrewbovbel/Developer/rust_mazer/target/debug/deps/rust_mazer-3adde574d2ea8e65.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_mazer.d
@@ -0,0 +1,1 @@
+/Users/andrewbovbel/Developer/rust_mazer/target/debug/rust_mazer: /Users/andrewbovbel/Developer/rust_mazer/src/main.rs
